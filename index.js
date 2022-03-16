9c36e2248a7c0c446f43ee7a2902ade4254bb2a0
Mohammed Eltahir <mseeltahir@gmail.com>
2022-03-16 12:16:42 +0200

    connected to Telegram API and made basic commands

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-require("dotenv").config();
 const axios = require("axios").default;
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
 
-axios({
-    url: "https://api.twitter.com/2/users/by/username/nodejs",
-    headers: { Authorization: `Bearer ${process.env.BEARER_TOKEN}` },
-}).then((res) => console.log(res));
+// axios({
+//     url: "https://api.twitter.com/2/users/by/username/nodejs",
+//     headers: { Authorization: `Bearer ${process.env.BEARER_TOKEN}` },
+// }).then((res) => console.log(res));
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply("Hello human :)"));
+bot.command("list", (ctx) => ctx.reply("List of accounts you're following"));
+bot.command("follow", (ctx) => ctx.reply("Enter username"));
+bot.command("unfollow", (ctx) => ctx.reply("Enter username"));
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
