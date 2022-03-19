f7da20411e1882364e8d2ed6d30a742eb47771d1
Mohammed Eltahir <mseeltahir@gmail.com>
2022-03-19 11:47:46 +0200

    made all command-handlers use the database

--- index.js
+++ index.js
@@ -11,31 +11,7 @@ mongoose
 
 const bot = new TeleBot(process.env.BOT_TOKEN);
 
-const data = [
-    {
-        // userID: parseInt(process.env.MY_ID),
-        // currentCommand: "None",
-        // following: [],
-    },
-];
-
-const findUser = (id) => {
-    const index = data.findIndex((user) => user.userID === id);
-    return {
-        found: index === -1 ? false : true,
-        index: index,
-    };
-};
-
-const addUser = (id, ...rest) => {
-    data.push({
-        userID: id,
-        currentCommand: rest.length ? rest[0] : "None",
-        following: [],
-    });
-};
-
-bot.on("/start", async (msg) => {
+bot.on(["/start", "/help"], async (msg) => {
     const checkUser = await UserHelper.find(msg.from.id);
     if (!checkUser.found) {
         const user = await UserHelper.add(msg.from);
@@ -47,29 +23,45 @@ Choose a command to do one of the following:\n
 /unfollow - to unfollow an account`);
 });
 
-bot.on("/follow", (msg, props) => {
-    const check = findUser(msg.from.id);
-    data[check.index].currentCommand = "follow";
-    console.log(data);
+bot.on("/follow", async (msg) => {
+    const checkUser = await UserHelper.find(msg.from.id);
+    const user = checkUser.user;
+    user.currentCommand = "follow";
+    await user.save();
     msg.reply.text("Enter Twitter handle below (must start with @):");
 });
 
-bot.on("/unfollow", (msg, props) => {
-    const check = findUser(msg.from.id);
-    data[check.index].currentCommand = "unfollow";
-    console.log(data);
+bot.on("/unfollow", async (msg) => {
+    const checkUser = await UserHelper.find(msg.from.id);
+    const user = checkUser.user;
+    user.currentCommand = "unfollow";
+    await user.save();
     msg.reply.text("Enter Twitter handle below (must start with @):");
 });
 
-bot.on("/list", (msg, props) => {
-    const check = findUser(msg.from.id);
-    msg.reply.text("List of the accounts you're following:");
+bot.on("/list", async (msg) => {
+    try {
+        const checkUser = await UserHelper.find(msg.from.id);
+        const user = checkUser.user;
+        msg.reply.text(`List of the accounts you're following:`);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
-bot.on("text", (msg, props) => {
-    const check = findUser(msg.from.id);
-    if (check.found) {
-        const user = data[check.index];
+bot.on("text", async (msg) => {
+    const checkUser = await UserHelper.find(msg.from.id);
+    const user = checkUser.user;
+
+    if (
+        ["/start", "/follow", "/unfollow", "/list", "/help"].includes(
+            msg.text
+        ) ||
+        msg.text[0] === "@"
+    ) {
+        return;
+    }
+    if (checkUser.found) {
         if (
             user.currentCommand === "follow" ||
             user.currentCommand === "unfollow"
@@ -80,26 +72,22 @@ bot.on("text", (msg, props) => {
             }
         }
     }
-    if (
-        ["/start", "/follow", "/unfollow", "/list"].includes(msg.text) ||
-        msg.text[0] === "@"
-    ) {
-        return;
-    }
     msg.reply.text("Use available commands only.");
 });
 
-bot.on(/^@/, (msg, props) => {
-    const check = findUser(msg.from.id);
-    const user = data[check.index];
+bot.on(/^@/, async (msg) => {
+    const checkUser = await UserHelper.find(msg.from.id);
+    const user = checkUser.user;
     if (user.currentCommand === "follow") {
         msg.reply.text(`Followed ${msg.text}`);
         // follow
         user.currentCommand = "None";
+        await user.save();
     } else if (user.currentCommand === "unfollow") {
         msg.reply.text(`Unfollowed ${msg.text}`);
         // unfollow
         user.currentCommand = "None";
+        await user.save();
     } else {
         msg.reply.text("Use a valid command before entering a username.");
     }
