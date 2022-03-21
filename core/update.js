bd79bac88710fdcf4e4ca12b73443504e69b009d
Mohammed Eltahir <mseeltahir@gmail.com>
2022-03-22 01:25:10 +0200

    implemented the core functionality of sending tweets to users

---
+++ core/update.js
@@ -0,0 +1,29 @@
+const { fetchTweets } = require("../helpers/twitter.h");
+const User = require("../models/User");
+
+require("dotenv").config();
+
+async function update(bot) {
+    const tweets = await fetchTweets();
+    console.log(tweets);
+    const users = await User.find().populate("following");
+    for (let user of users) {
+        for (let tu of user.following) {
+            if (tu._id in tweets) {
+                tuTweets = tweets[tu._id];
+                for (let tweet of tuTweets) {
+                    await bot.sendMessage(
+                        user._id,
+                        `<b>${tu.name}</b> (<a
+                            href="https://twitter.com/${tu.username}">@${tu.username}</a>)\n\n${tweet.text}\n\n<a \
+href="https://twitter.com/${tu.username}/status/${tweet.id}">Tweet \
+Link</a>`,
+                        { parseMode: "HTML", webPreview: false }
+                    );
+                }
+            }
+        }
+    }
+}
+
+module.exports = update;
