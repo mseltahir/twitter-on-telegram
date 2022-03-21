679a3461b5a24fe63fae4311c26d25a95f086f12
Mohammed Eltahir <mseeltahir@gmail.com>
2022-03-21 10:37:02 +0200

    implemented find twitter user from twitter api

--- helpers/twitter.h.js
+++ helpers/twitter.h.js
@@ -3,15 +3,35 @@ require("dotenv").config();
 
 const client = new TwitterApi(process.env.BEARER_TOKEN);
 
-(async function () {
-    const nodejsTweets = await client.v2.userTimeline("91985735");
-    console.log(nodejsTweets.tweets);
-    console.log(
-        "--------------------------------------------------------------------------------------"
-    );
-    await nodejsTweets.fetchNext();
-    console.log(nodejsTweets.tweets);
-    // for (let tweet of nodejsTweets) {
-    //     console.log(tweet.text);
-    // }
-})();
+// when a user calls the follow command, I need a funciton to return the
+// twitter id of the user to be saved in the database this funciton will
+// receive the username and save it with the retrieved id and returns
+// true if the request was successful or false if the request was not
+
+async function findTwitterUser(username) {
+    const user = await client.v2.usersByUsernames(username);
+    if (user.errors) {
+        return {
+            found: false,
+            data: null,
+        };
+    } else {
+        return {
+            found: true,
+            data: user.data[0],
+        };
+    }
+}
+
+// (async function () {
+//     const nodejsTweets = await client.v2.userTimeline("91985735");
+//     console.log(nodejsTweets.tweets);
+//     console.log(
+//         "--------------------------------------------------------------------------------------"
+//     );
+//     await nodejsTweets.fetchNext();
+//     console.log(nodejsTweets.tweets);
+//     // for (let tweet of nodejsTweets) {
+//     //     console.log(tweet.text);
+//     // }
+// })();
